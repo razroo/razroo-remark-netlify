cfd1291d7ac6bbdbd339fa4631ceff58b23ac52c
Charlie Greenman <charlie@razroo.com>
2021-03-31 10:57:01 +0300

    Benchmark progress towards updating the plugin.ts file.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,5 +1,5 @@
 import visit from 'unist-util-visit';
-import {removeTitleTags} from "./removeTitleTags";
+import {matchNetlifyTitleTag, transformTitleTags} from "./removeTitleTags";
 
 
 export function removeTitleTagsPlugin({}) {
@@ -14,13 +14,13 @@ export function removeTitleTagsPlugin({}) {
 
       promises.push(
         (async () => {
-          const match = removeTitleTags(paragraph);
+          const match = matchNetlifyTitleTag(paragraph);
 
 
           if (match) {
             parent.children.splice(parent.children.indexOf(paragraph), 1, {
               type: 'paragraph',
-              value: tex.replace(titleText, replaceTitleTags(match,tree));
+              value: transformTitleTags(match);
             });
           }
         })(),
@@ -29,18 +29,4 @@ export function removeTitleTagsPlugin({}) {
 
     await Promise.all(promises);
   };
-}
-
-function replaceTitleTags(match,text) {
-  let titleText = match.toString()
-  let newTitleText
-  let modifiedFile
-  const arrayOfTitleText = titleText.match(/title:(.*)/);
-  if (arrayOfTitleText) {
-    console.log("trying new regex:" + arrayOfTitleText[1]);
-    let newTitleText = '# ' + arrayOfTitleText[1];
-    modifiedFile = text.replace(titleText, newTitleText);
-
-  }
-  return newTitleText
 }
